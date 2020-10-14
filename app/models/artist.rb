024f89f836e356cc13c06ce257e916dd444f6fba
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-14 04:45:07 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,11 +3,11 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.songs.first
+    self.songs.first.genre
   end
 
   def song_count
-    self.songs.all.count
+    self.songs.count
   end
 
   def genre_count
